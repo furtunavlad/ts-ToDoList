6091681cf4538ec2911e0615052c1f3aac1e4238
Vlad Furtuna <144479033+furtunavlad@users.noreply.github.com>
2023-12-09 13:18:39 +0200

    main.ts

--- ts-ToDoList/src/main.ts
+++ ts-ToDoList/src/main.ts
@@ -1,1 +1,18 @@
 import './css/style.css'
+import FullList from './model/FullList'
+import ListItem from './model/ListItem'
+import ListTemplate from './templates/ListTemplate'
+
+const initApp = (): void => {
+  const fullList = FullList.instance
+  const template = ListTemplate.instance
+
+  const itemEntryForm = document.getElementById("itemEntryForm") as HTMLFormElement
+  itemEntryForm.addEventListener("submit", (event: SubmitEvent): void => {
+    event.preventDefault()
+
+  })
+  
+}
+
+document.addEventListener("DOMContentLoaded", initApp)
